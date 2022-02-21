d3b2b40454025da51b7e9d193d6853432d349474
Oded Lazar <odedl@talon-sec.com>
2022-02-21 16:20:37 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ async function run() {
   try {
     const
       titleRegex = new RegExp(core.getInput('title-regex', { required: true }),
-                              core.getInput('title-regex-flags') || 'g'),
+        core.getInput('title-regex-flags') || 'g'),
       bodyRegex = new RegExp(core.getInput('body-regex', { required: true }),
-                             core.getInput('body-regex-flags') || 'g'),
+        core.getInput('body-regex-flags') || 'g'),
       errorMessage = core.getInput('error-message') || `Please fix your PR title to match "${titleRegex.source}" with "${titleRegex.flags}"`,
       title = github.context!.payload!.pull_request!.title,
-      body = github.context!.payload!.pull_request!.body;
+      body = github.context!.payload!.pull_request!.body ?? "";
 
     core.info(`Checking "${titleRegex.source}" with "${titleRegex.flags}" flags against the PR title: "${title}"`);
     core.info(`Checking "${bodyRegex.source}" with "${bodyRegex.flags}" flags against the PR body: "${body}"`);
-    let match = titleRegex.exec(title) || bodyRegex.exec(!!body ? body : "")
-    if (match == null) {
+    let match = titleRegex.exec(title) || bodyRegex.exec(body)
+    if (!match) {
       core.setFailed(errorMessage);
       return;
     }
@@ -23,15 +23,15 @@ async function run() {
     const client: github.GitHub = new github.GitHub(core.getInput('github-token'));
     const pr = github.context.issue;
 
-    const ticket  = match.groups['ticket'];
+    const ticket = match?.groups['ticket'] as string;
     const newBody = body?.replace(ticket, `https://talon-sec.atlassian.net/browse/${ticket}`);
-
     client.pulls.update({
       owner: pr.owner,
       repo: pr.repo,
       pull_number: pr.number,
       body: newBody
     });
+
   } catch (error) {
     core.setFailed(error.message);
   }
